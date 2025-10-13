f1185d2e94f6a5a76b765141c7ad7d7c4f251aa5
Harsh-Vdn25 <hars.vdn16@gmail.com>
2025-10-13 07:53:07 +0530

    Client Side Socket Connection(WIP)

---
+++ FE/src/app/websocket/chat.ts
@@ -0,0 +1,48 @@
+"use client";
+import { RefObject } from "react";
+
+export interface sendMessageType {
+  socketRef: RefObject<WebSocket | null>;
+  roomName: string;
+  token:string;
+  message:string;
+  isJoin: boolean;
+  setisJoin: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export default async function sendMessage({
+  socketRef,
+  roomName,
+  token,
+  message,
+  isJoin,
+  setisJoin,
+}: sendMessageType) {
+  if(socketRef.current?.readyState===WebSocket.OPEN){
+    if (isJoin) {
+    socketRef.current?.send(
+      JSON.stringify({
+        type: "join",
+        roomName: roomName,
+        token: token,
+      })
+    );
+    setisJoin(false);
+  } else {
+    socketRef.current?.send(
+      JSON.stringify({
+        type: "chat",
+        roomName:roomName,
+        message: message
+      })
+    );
+  }
+  }else{
+    socketRef.current?.addEventListener('open',()=>socketRef.current?.send(JSON.stringify({
+        type: "join",
+        roomName: roomName,
+        token: token,
+      })));
+      setisJoin(false)
+  }
+}
